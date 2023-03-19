2fe52ec1bdeb11899c13a94e61dbfc1e035a8008
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2023-03-19 11:52:48 +0530

    working on design  🔥

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// const body=document.querySelector('body');
+// const containerDiv= document.createElement('div');
+// containerDiv.classList.add('container')
+// body.appendChild(containerDiv)
+
+// const mainBox= document.createElement('div')
+// mainBox.classList.add('main-box')
+// containerDiv.appendChild(mainBox)
+
+// const box=document.createElement('div');
+// box.classList.add('box')
+// mainBox.appendChild(box)
+
+// const box1=document.createElement('div');
+// box1.classList.add('box')
+// mainBox.appendChild(box1)
+// const box2=document.createElement('div');
+// box2.classList.add('box')
+// mainBox.appendChild(box2)
